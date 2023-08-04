9135bc91eff273881d981dc3d8e1895ea72aa73c
Ghanshyam Kumar Mahato <48532181+shyamghan17@users.noreply.github.com>
2023-08-04 22:40:18 +0100

    ..

---
+++ Python/fileObject.py
@@ -0,0 +1,4 @@
+def hello_fun():
+    print('hello world')
+
+    hello_fun()
---
+++ Python/function.py
@@ -0,0 +1,17 @@
+# courses = ['Math', 'Physics', 'History', 'Computer', 'Art']
+# courses2 = ['Music', 'Acting', 'Reacting', 'Biology', 'Dancing', 'Kites', 'Yam raj']
+# courses.extend(courses2)
+# courses.sort()
+# numberes = [1, 4, 9, 2, 6, 8, 3, 5, 7]
+# poped = sum(numberes)
+# # print(poped)
+# # print('Ball' in courses)
+# course_str = '%'.join(courses)
+#
+# print(course_str)
+# #
+# for index, cours in enumerate(course_str):
+#     print(index, cours)
+
+course = {'Math', 'Physics', 'History', 'Computer', 'Art', 'Math'}
+print('Math' in course)
