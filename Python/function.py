9393279a825c017e67d92c3353ca2960db43bd82
Ghanshyam Kumar Mahato <48532181+shyamghan17@users.noreply.github.com>
2023-08-15 12:14:24 +0100

    done

--- Python/function.py
+++ Python/function.py
@@ -1,17 +1,8 @@
-# courses = ['Math', 'Physics', 'History', 'Computer', 'Art']
-# courses2 = ['Music', 'Acting', 'Reacting', 'Biology', 'Dancing', 'Kites', 'Yam raj']
-# courses.extend(courses2)
-# courses.sort()
-# numberes = [1, 4, 9, 2, 6, 8, 3, 5, 7]
-# poped = sum(numberes)
-# # print(poped)
-# # print('Ball' in courses)
-# course_str = '%'.join(courses)
-#
-# print(course_str)
-# #
-# for index, cours in enumerate(course_str):
-#     print(index, cours)
+# def hello_func(greeting, name='You'):
+#     return '{}, {}'.format(greeting, name)
+# print(hello_func('Hi', 'hello'))
 
-course = {'Math', 'Physics', 'History', 'Computer', 'Art', 'Math'}
-print('Math' in course)
+def stuent_info(*args, **kwargs):
+    print(args)
+    print(kwargs)
+stuent_info('Math', 'Art', name='Ghanshyam', age=28)
