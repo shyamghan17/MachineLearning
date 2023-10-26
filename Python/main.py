0d37a6fa622748937636aad803d7612a99f730ab
Ghanshyam Kumar Mahato <48532181+shyamghan17@users.noreply.github.com>
2023-10-26 15:59:44 +0100

    done

--- Python/main.py
+++ Python/main.py
@@ -57,9 +57,45 @@
 # else:
 #     viking_song = "Spam" * spam_amount
 # print(viking_song
-x = 12
-# x is a real number, so its imaginary part is 0.
-print(x.imag)
-# Here's how to make a complex number, in case you've ever been curious:
-c = 12 + 3j
-# print(c.imag)
+# x = 12
+# # x is a real number, so its imaginary part is 0.
+# print(x.imag)
+# # Here's how to make a complex number, in case you've ever been curious:
+# c = 12 + 3j
+# # print(c.imag)
+
+
+# racers = ["Mario", "Bowser", "Luigi"]
+# r = []
+#
+#
+# def purple_shell(racers):
+#     for i in reversed(racers):
+#
+#         return i
+# print(purple_shell())
+# fruit ='mango'
+# startingIndex = len(fruit) - 1
+# stopindex = -1
+# step = -1
+#
+# for i in range(len(fruit)-1, -1, -1):
+#     print(fruit[i])
+#
+#print(' ')
+L = [1, 2, 3, 4]
+thresh = 2
+def elementwise_greater_than(L, thresh):
+    """Return a list with the same length as L, where the value at index i is
+    True if L[i] is greater than thresh, and False otherwise.
+
+    >>> elementwise_greater_than([1, 2, 3, 4], 2)
+    [False, False, True, True]
+    """
+    elementwise_greater_than = []
+    for i in L:
+        if L[i]>thresh:
+            elementwise_greater_than.append(True)
+    return elementwise_greater_than.append(False)
+print(elementwise_greater_than(L, thresh))
+#%%
