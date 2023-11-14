c9490cae05e51059217d46a8e7aa99e0baa0b7ca
Ghanshyam Kumar Mahato <48532181+shyamghan17@users.noreply.github.com>
2023-11-14 17:08:44 +0000

    done

--- Python/pythonclass.py
+++ Python/pythonclass.py
@@ -1,15 +1,16 @@
 class Employee:
+
+
+
+
     def __int__(self, first, last, pay):
         self.first = first
         self.last = last
         self.pay = pay
-        self.email = first + '.' + 'last' + '@kushawaha.com'
+        self.email = first + '.' + last + '@kushawaha.com'
 
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
 
-
-emp_1 = Employee('ghanshyam', 'mahato', 100000)
-emp_2 = Employee('manha', 'singh', 120000)
-
-print(emp_1.fullName())
+    emp_1 = Employee('ram', 'shyam', 100000)
+    emp_2 = Employee('manisha', 'singh', 120000)
